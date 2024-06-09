ffbcb5a4dea809574702867f2fd388d6d8f68a71
Ubaid Raza <154611273+ubaiidraza@users.noreply.github.com>
2024-06-09 17:53:38 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,98 +1,38 @@
-const form = document.querySelector('#form');
-const username = document.querySelector('#username');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
+
+"https://the-trivia-api.com/v2/questions"
 
 
-form.addEventListener('submit', (event)=> {
-    event.preventDefault();
-
-    validateInputs();
-});
-
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
+function myInput(){
+    let x = document.querySelector('#password')
+    if(x.type === 'password'){
+        x.type = 'text'
+    }else {
+        x.type = 'password'
+    }
 }
 
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
-
-const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    
-
-    if(usernameValue === '') {
-        setError(username, 'Username is required');
-    } else {
-        setSuccess(username);
-    }
-
-    if(emailValue === '') {
-        setError(email, 'Email is required');
-    } else if (!isValidEmail(emailValue)) {
-        setError(email, 'Provide a valid email address');
-    } else {
-        setSuccess(email);
-    }
-
-    if(passwordValue === '') {
-        setError(password, 'Password is required');
-    } else if (passwordValue.length < 8 ) {
-        setError(password, 'Password must be at least 8 character.')
-    } else {
-        setSuccess(password);
-    }
-
-   
-
-};
+let email = document.querySelector('#email')
+let password = document.querySelector('#password')
+let form = document.querySelector('form')
 
 
+let getLocal = JSON.parse(localStorage.getItem('sendLocal'))
+console.log(getLocal);
+let emailCheck = getLocal.map(item => {
+    return item.email
+})
 
 
-function saveData()
-{
-let name,email,psw;
-name=document.getElementById("username").value;
-email=document.getElementById("email").value;
-
-psw=document.getElementById("password").value;
-
-localStorage.setItem("name",name)
-localStorage.setItem("email",email)
-localStorage.setItem("password",psw)
-}
-          
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    getLocal.filter(item => {
+        if(item.email === email.value && item.password === password.value){
+            alert('Login SuccesFull')
+            window.location = 'quiz.html'
+        }else if(item.email === email.value){
+            alert('Password Incorrect!')
+        }else {
+            alert('please Register First')
+        }
+    })
+})
